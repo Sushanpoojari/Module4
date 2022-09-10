d1b39629e6e6eba2612009633a9eb3506d83d417
Sushanpoojari <91779688+Sushanpoojari@users.noreply.github.com>
2022-09-10 15:30:58 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+    var names = ["Sushan", "John", "Tom", "Jim", "Sherlock", "Jimmy", "Robert", "Johnny"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } 
+      else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    })();
